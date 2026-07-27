cc0e951c3d0fb711c8d095720e45bc343e99454e
Reina-mimi <Horlahwunmi4@gmail.com>
2026-07-27 14:14:01 +0100

    404

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const Not_Found = () => {
+  return (
+    <div className="flex p-4 text-2xl font-bold text-center items-center justify-center">
+      Sorry, I am still working on this route
+    </div>
+  );
+};
+
+export default Not_Found;
