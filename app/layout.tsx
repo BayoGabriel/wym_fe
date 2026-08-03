5c3a1640f36e756473feefd409888c773b2ec069
Reina-mimi <Horlahwunmi4@gmail.com>
2026-08-03 11:23:33 +0100

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Wynmet",
   description: "",
+  icons: {
+    icon: "/assets/images/logo.png",
+  },
 };
 
 export default function RootLayout({
