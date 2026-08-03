e11d65c5ab259742cf15c7c579da95eab89d8453
Reina-mimi <Horlahwunmi4@gmail.com>
2026-08-03 09:05:17 +0100

    fx

--- src/views/about_view/about_view_components/Core_Values.tsx
+++ src/views/about_view/about_view_components/Core_Values.tsx
@@ -7,8 +7,8 @@ const Core_Values = () => {
       <p className="font-[500] font-aeonik lg:text-[60px] text-blackb text-[30px] xl:w-[100%]  mx-auto  py-[24px] flex lg:leading-[72px] xl:pl-[0px] pl-[24px]">
         Our Core Values
       </p>
-      <div className="flex flex-col xmd:flex-row gap-4 md:h-[500px] lg:h-[700px]">
-        <div className="relative bg-gradient-to-tr from-[#0C2648] to-[#09172B] xmd:h-auto xmd:w-[33.33%] xl:w-[43%] grid gap-8 xmd:gap-[5rem] lg:gap-[7rem] overflow-hidden rounded-2xl text-white px-8 pt-8">
+      <div className="flex flex-col md:flex-row gap-4 md:h-[500px] lg:h-[700px]">
+        <div className="relative bg-gradient-to-tr from-[#0C2648] to-[#09172B] md:h-auto md:w-[33.33%] xl:w-[43%] grid gap-8 md:gap-[5rem] lg:gap-[7rem] overflow-hidden rounded-2xl text-white px-8 pt-8">
           <Image
             src={AboutImage}
             alt=""
@@ -28,8 +28,8 @@ const Core_Values = () => {
           </div>
         </div>
 
-        <div className="xmd:w-[66.66%] xl:flex-1 xl:flex-col xmd:flex gap-4 ">
-          <div className="bg-[#0a40ed] grid gap-8 xmd:gap-[5rem] xmd:h-full overflow-hidden text-white px-8 pt-8 rounded-2xl">
+        <div className="md:w-[66.66%] xl:flex-1 xl:flex-col md:flex gap-4 ">
+          <div className="bg-[#0a40ed] grid gap-8 md:gap-[5rem] md:h-full overflow-hidden text-white px-8 pt-8 rounded-2xl">
             <div className="xl:w-[60%] ">
               <p className="text-[26px] font-[500] font-aeonik leading-[30px] ">
                 Inclusivity
@@ -43,11 +43,11 @@ const Core_Values = () => {
               <Image
                 src={AboutImage2}
                 alt=""
-                className="mx-auto mb-[-5rem] xmd:mb-0 lg:absolute bottom-[-5px] right-[-100px] lg:bottom-[-13.5rem] object-contain "
+                className="mx-auto mb-[-5rem] md:mb-0 lg:absolute bottom-[-5px] right-[-100px] lg:bottom-[-13.5rem] object-contain "
               />
             </div>
           </div>
-          <div className="bg-[#bfdefa] grid gap-8 xmd:gap-[5rem] h-[50%] xmd:h-full overflow-hidden text-[#101828] px-8 pt-8 rounded-2xl">
+          <div className="bg-[#bfdefa] grid gap-8 md:gap-[5rem] h-[50%] md:h-full overflow-hidden text-[#101828] px-8 pt-8 rounded-2xl">
             <div className="xl:w-[60%]">
               <p className="text-[26px] font-[500] font-aeonik leading-[30px] ">
                 Reliability Redefined
@@ -61,7 +61,7 @@ const Core_Values = () => {
               <Image
                 src={AboutImage3}
                 alt=""
-                className="w-[50%] mx-auto mb-[-5rem] xmd:mb-0 lg:absolute bottom-0 right-0 lg:bottom-[-1.5rem] object-contain "
+                className="w-[50%] mx-auto mb-[-5rem] md:mb-0 lg:absolute bottom-0 right-0 lg:bottom-[-1.5rem] object-contain "
               />
             </div>
           </div>
